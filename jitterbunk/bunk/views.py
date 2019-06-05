5428066c0744fd9d4ba85e05ecd0ffb0706a5866
Mighty Chen <mighty@codehs.com>
2019-06-05 16:07:24 -0700

    jitterbunk but you can only send bunks as Mighty

--- jitterbunk/bunk/views.py
+++ jitterbunk/bunk/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.db.models import Q
+from .models import Bunk, User
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.utils import timezone
 # Create your views here.
-def index(response):
-    return HttpResponse("hello world")
+
+def index(request):
+    latest_bunks = Bunk.objects.order_by('-time')[:10]
+    context = {'latest_bunks': latest_bunks}
+    return render(request, 'bunk/index.html', context)
+
+def profile(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    recent_bunks = Bunk.objects.order_by('-time').filter(
+                        Q(from_user=user) | Q(to_user=user)
+                        )[0:10]
+
+    bunk_score = 0
+    positive_bunk_score = Bunk.objects.filter(from_user=user).count()
+    negative_bunk_score = Bunk.objects.filter(to_user=user).count()
+    bunk_score = positive_bunk_score - negative_bunk_score
+    context = {
+        'user' : user,
+        'recent_bunks' : recent_bunks,
+        'bunk_score' : bunk_score,
+        'total_bunks' : positive_bunk_score
+    }
+    return render(request, 'bunk/profile.html', context)
+
+def bunk(request):
+    to_user = User.objects.filter(pk=request.POST['to_user'])[0]
+    from_user = User.objects.filter(pk=request.POST['from_user'])[0]
+    new_bunk = Bunk(from_user=from_user, to_user=to_user, time=timezone.now())
+    new_bunk.save()
+    context = {
+        'just_bunked' : to_user
+    }
+    return HttpResponseRedirect('/', context)
---
+++ jitterbunk/populate.py
@@ -0,0 +1,25 @@
+import django
+django.setup()
+
+from bunk.models import User, Bunk
+from django.utils import timezone
+
+import random
+# Delete Everything
+users = User.objects.all()
+for u in users:
+    u.delete()
+
+new_users = ["John", "Calvin", "Andy", "Zach", "Mighty", "Robert"]
+for nu in new_users:
+    u = User(user=nu, photo="default")
+    u.save()
+
+for i in range(10000):
+    people = random.sample(set(User.objects.all()),2)
+    b = Bunk(
+            from_user = people[0],
+            to_user = people[1],
+            time = timezone.now()
+        )
+    b.save()
